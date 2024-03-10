ed358558363745699bca60e89d074e69e72006c2
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2024-03-10 18:16:22 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(page_title="🦙💬 CareNet LLAMA 2 Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
